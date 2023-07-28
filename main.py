c4cb1093bc8a585b960d66fb6556928d9e8d3865
pickles976 <sebaslogo@gmail.com>
2023-07-28 10:15:53 -0500

    Added stuff

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import unittest
-
-class Dollar:
-    def __init__(self, amount):
-        self.amount = amount
-
-    def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+from money import Money
+from portfolio import Portfolio
 
 class TestMoney(unittest.TestCase):
+
     def testMultiplication(self):
-        fiver = Dollar(5)
-        tenner = fiver.times(2)
-        self.assertEqual(10, tenner.amount)
+        tenEuros = Money(10, "EUR")
+        twentyEuros = Money(20, "EUR")
+        self.assertEqual(twentyEuros, tenEuros.times(2))
+
+    def testDivision(self):
+        originalMoney = Money(4002, "KRW")
+        actualMoneyAfterDivision = originalMoney.divide(4)
+        expectedMoneyAfterDivision = Money(1000.5, "KRW")
+        self.assertEqual(expectedMoneyAfterDivision.amount, actualMoneyAfterDivision.amount)
+        self.assertEqual(expectedMoneyAfterDivision.currency, actualMoneyAfterDivision.currency)
+
+    def testAddition(self):
+        fiveDollars = Money(5, "USD")
+        tenDollars = Money(10, "USD")
+        fifteenDollars = Money(15, "USD")
+        portfolio = Portfolio()
+        portfolio.add(fiveDollars, tenDollars)
+        self.assertEqual(fifteenDollars, portfolio.evaluate("USD"))
 
 if __name__ == '__main__':
     unittest.main()
